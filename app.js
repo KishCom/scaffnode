762d83c74430c615f53863ef56813c2eb5f3c699
Andrew <kishcom@cryptonectar.(none)>
2012-01-17 15:48:00 -0500

    Swapped mustache templates out for swig templates. Updated some packages, README and TODO.

--- app.js
+++ app.js
@@ -6,26 +6,30 @@
 */
 
 /**  Depends  **/
-var express = require('express');
-var routes = require('./routes');
-var site = module.exports = express.createServer();
+var express = require('express'),
+    swig = require('swig'),
+    routes = require('./routes'),
+    site = module.exports = express.createServer();
 
 
-/**  Configuration  **/
+//**  Configuration  **/
 site.configure(function(){
     var PUBLIC_FOLDER = __dirname + '/public';
 
-    //Setup views
+    //Setup views and swig templates
+    swig.init({root: __dirname + '/views', allowErrors: true});
     site.set('views', __dirname + '/views');
-    site.set('partials', __dirname + '/views/partials');
-    site.set('view engine', 'mustache');
+    site.set('view engine', 'html');
+    site.register(".html", require('swig'));
+    site.set('view cache', true);
+    site.set('view options', {layout: false});
+    
     site.use(express.cookieParser());
-    site.use(express.session({secret: 'iamasecretkeydonttellanyone', key: 'express.sid'}));
-    site.register(".mustache", require('stache'));
+    site.use(express.session({secret: 'iamasecretkeydonttellanyone', key: 'express.sid'})); //Make sure you update your secret key here
     //LESS compiler
     site.use(express.compiler({ src: PUBLIC_FOLDER, enable: ['less']}));
     //The rest of our static-served files
-    site.use(express.static(PUBLIC_FOLDER)); //Static is a reserved word! ExpressJS needs to fix this
+    site.use(express.static(PUBLIC_FOLDER));
 
     //Middlewares
     site.use(express.bodyParser()); //Connect alias
@@ -37,25 +41,12 @@ site.configure(function(){
 //Developer mode specific
 site.configure('dev', function(){
     //Show errors
-    site.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+    site.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 //Live mode specific
 site.configure('live', function(){
     //Hide errors
-    site.use(express.errorHandler()); 
-});
-
-
-/** mustache helpers **/
-site.helpers({
-  helloworld: function(req, res){
-    return 'hello world';
-  }
-});
-site.dynamicHelpers({
-  hellopage: function(req, res){
-    return req.url;
-  }
+    site.use(express.errorHandler());
 });
 
 
@@ -64,5 +55,5 @@ site.get('/', routes.index);
 
 
 /**  Start Server  **/
-site.listen(8080);
+site.listen(8888);
 console.log("Express listening on port %d in %s mode", site.address().port, site.settings.env);
