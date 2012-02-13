1ec2e59e5f34b9625f99f84054c2bd4ca301a5a5
Andrew <kishcom@cryptonectar.(none)>
2012-02-12 23:42:02 -0500

    Changes to dir layout. Added dead simple pick script

---
+++ pick.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Pick what app you want
+# useage: ./pick sio.js
+#    - replaces app.js with sio.js
+#    - removes other app files
+#    - truncates README and TODO files
+if [ -e $1 ] ; then
+echo `mv $1 app.js`
+echo "" > README
+echo "" > TODO
+fi
