1372dc605d2d1fa971a5458d9c8a8593ac79f99f
Andrew Kish <andrewk@tribalworldwide.ca>
2015-04-15 16:02:21 -0400

    Merge complete

--- app.js
+++ app.js
@@ -10,19 +10,19 @@ var express = require("express"),
     cookieParser = require("cookie-parser"),
     bodyParser = require("body-parser"),
     expressSession = require("express-session"),
-    multer = require('multer'),
-    i18n = require('i18n'),
-    hpp = require('hpp'),
+    hpp = require("hpp"),
+    multer = require("multer"),
+    i18n = require("i18n"),
     Routes = require("./routes"), routes,
     Utils = require("./utils"), utils,
     site = module.exports = express();
 
 // Load configuration details based on your environment
 var config, NODE_ENV;
-var packagejson = require('./package');
+var packagejson = require("./package");
 var isTestMode = false;
 if (process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "live" || process.env.NODE_ENV === "test"){
-    // If we're in test mode, just set a flag on the config object and switch the NODE_ENV to "dev"
+    // If we'"'re in test mode, just set a flag on the config object and switch the NODE_ENV to "dev"
     //
     if (process.env.NODE_ENV === "test"){
         isTestMode = true;
@@ -46,20 +46,20 @@ if (process.env.NODE_ENV === "dev" || process.env.NODE_ENV === "live" || process
         "connect-redis": "~2.0.x",
     // Uncomment this stuff (and down where expressSession is initiated too!):
     var redis = require("redis");
-    var redisStore = require('connect-redis')(expressSession),
+    var redisStore = require("connect-redis")(expressSession),
         redis_client = redis.createClient(config.redisPort, config.redisHost, {detect_buffers: true}); // Assumes redis is running on localhost on default port
-    site.set('redis', redis_client);
+    site.set("redis", redis_client);
 */
 
 //Setup views and nunjucks templates
-nunjucks.configure('views', {
+nunjucks.configure("views", {
     autoescape: true,
     express: site
 });
 site.set("view engine", "html");
 site.set("views", __dirname + "/views");
-site.enable('trust proxy');
-site.disable('x-powered-by');
+site.enable("trust proxy");
+site.disable("x-powered-by");
 
 //The rest of our static-served files
 site.use(express.static(__dirname + "/public"));
@@ -74,8 +74,8 @@ log = bunyan.createLogger(
         }
         // Setup an addional logger with ease
         /*,{
-            level: 'warn',
-            stream: new utils(), // looks for 'write' method. https://github.com/trentm/node-bunyan
+            level: "warn",
+            stream: new utils(), // looks for "write" method. https://github.com/trentm/node-bunyan
         }*/
         ]}
     );
@@ -86,9 +86,9 @@ utils = new Utils(site, log, config);
 
 /** Middlewares! **/
 site.use(multer({
-    dest: '/tmp/',
+    dest: "/tmp/",
     rename: function (fieldname, filename) {
-        return filename.replace(/\W+/g, '-').toLowerCase();
+        return filename.replace(/\W+/g, "-").toLowerCase();
     }
 }));
 site.use(bodyParser.urlencoded({extended: true}));
@@ -100,7 +100,7 @@ site.use(expressSession({   secret: config.sessionSecret,
                             saveUninitialized: false,
                             resave: false,
                             //store: new redisStore({ client: redis_client }),
-                            cookie: {maxAge: new Date(Date.now() + 604800*1000), path: '/', httpOnly: true, secure: false}
+                            cookie: {maxAge: new Date(Date.now() + 604800*1000), path: "/", httpOnly: true, secure: false}
                         }));
 
 
@@ -108,9 +108,9 @@ site.use(expressSession({   secret: config.sessionSecret,
 i18n.configure({
   locales: config.supportedLocales,
   cookie: packagejson.name + "_lang.sid",
-  directory: __dirname + '/locales'
+  directory: __dirname + "/locales"
 });
-// Express helper (makes '__' functions available in templates)
+// Express helper (makes "__" functions available in templates)
 site.use(i18n.init);
 // Middleware helper, makes user language preferences sticky and watches for "lang" query variable
 site.use(utils.i18nHelper);
