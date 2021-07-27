0bc2af006648c657527124a37cb55a6dcedc04d2
Andrew Kish <akish@kishcom.com>
2021-07-27 11:41:27 -0400

    Update all packages.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,8 +1,9 @@
+const {GitRevisionPlugin} = require('git-revision-webpack-plugin');
 const path = require('path');
-const webpackGitBanner = require('./webpackGitBannerPlugin.js');
+const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 const isRunningDevMode = process.env.NODE_ENV === "dev";
 const outputPath = path.resolve(__dirname, '../public/dist');
@@ -12,8 +13,8 @@ const webpacked = {
     devtool: isRunningDevMode ? "eval-source-map" : false,
     entry: './src/app.js',
     output: {
-        filename: 'bundle.[hash].js',
-        chunkFilename: '[name].[hash].bundle.js',
+        filename: 'bundle.[contenthash].js',
+        chunkFilename: '[name].[contenthash].bundle.js',
         path: outputPath
     },
     module: {
@@ -41,31 +42,31 @@ const webpacked = {
             // Handle Fonts
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [{loader: 'file-loader'}]
+                type: 'asset/resource'
             },
             // Transpile JS
             {test: /\.js$/, exclude: /node_modules/, loader: "swc-loader"}
         ]
     },
     plugins: [
-        new webpackGitBanner("scaffnode tool chain"),
-        new MiniCssExtractPlugin({
-            filename: "bundle.[hash].css",
-            chunkFilename: "bundle.[hash].[id].css"
+        new GitRevisionPlugin(),
+        new webpack.BannerPlugin({
+            banner: "scaffnode - [name] [git-revision-hash]", // the banner as string or function, it will be wrapped in a comment
+            raw: false, // if true, banner will not be wrapped in a comment
+            entryOnly: true, // if true, the banner will only be added to the entry chunks
+            // test: string | RegExp | [string, RegExp], // Include all modules that pass test assertion.
+            // include: string | RegExp | [string, RegExp], // Include all modules matching any of these conditions.
+            // exclude: string | RegExp | [string, RegExp], // Exclude all modules matching any of these conditions.
         }),
-        new CleanWebpackPlugin([outputPath], {allowExternal: true}),
+        new MiniCssExtractPlugin({
+            filename: "bundle.[contenthash].css",
+            chunkFilename: "bundle.[contenthash].[id].css"
+        }),
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
     ],
     optimization: {
-        runtimeChunk: "single",
-        splitChunks: {
-            cacheGroups: {
-                vendor:{
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all'
-                }
-            }
-        }
+        runtimeChunk: "single"
     }
 };
 if (!isRunningDevMode){
